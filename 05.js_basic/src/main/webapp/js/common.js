0c8c8e381eed243eae40f17913c1119f99f90385
takingtheback <84066112+takingtheback@users.noreply.github.com>
2021-06-29 21:49:30 +0900

    외부 스크립트 사용
    현재 날짜 출력

--- 05.js_basic/src/main/webapp/js/common.js
+++ 05.js_basic/src/main/webapp/js/common.js
@@ -16,29 +16,32 @@
 
 function getCurrentDate() {
 	var currentDate = new Date();
-	var year = currentDate.getFullYear();
-	var month = currentDate.getMonth() + 1;
-	if(month < 10) {
-		month = "0" + month;
-	}
-	var date = currentDate.getDate();
-	if(date < 10) {
-		date = "0" + date;
-	}
-	
+	var year = ("0" + currentDate.getFullYear()).slice(-4);
+	var month = ("0" + (currentDate.getMonth()+ 1)).slice(-2);
+	var date = ("0" + currentDate.getDate()).slice(-2);
+		
 	currentDate = year + '-' + month + '-' + date;
 	console.log("currentDate: " + currentDate);
+	
+	return currentDate;
 }
 
-/* 현재 날짜 시간2자리:분2자리:초2자리 기본형식의 날짜 반환 함수 */
+/* 현재 날짜 시간2자리:분2자리:초2자리 기본형식의 날짜 반환 함수 
+	-- 01, 02, ... , 09, 010, 011, 012
+	-- ('0' + 시간) => 뒤에서 2자리 가져오기
+*/
 
 function getCurrentTime() {
+	var currentTime = new Date();
+	var hour = ("0" + currentTime.getHours()).slice(-2);
+	var minutes = ("0" + currentTime.getMinutes()).slice(-2);
+	var seconds = ("0" + currentTime.getSeconds()).slice(-2);
+	
+	currentTime = hour + ':' + minutes + ':' + seconds;
+	console.log("currentTime: " + currentTime);
+	
+	return currentTime;
+	
+		
 	
 }
-
-
-let hours = today.getHours(); // 시
-let minutes = today.getMinutes();  // 분
-let seconds = today.getSeconds();  // 초
-
-document.write(hours + ':' + minutes + ':' + seconds);
